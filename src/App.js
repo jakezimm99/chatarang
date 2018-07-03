fdc156fe1ab1dc819a2b5891eee6457a366af7ec
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-07-03 09:48:46 -0400

    Add Railway Fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+
+
+
 import './App.css'
+import Main from './Main'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Chatarang</h1>
-        </header>
+        <Main />
       </div>
     );
   }
