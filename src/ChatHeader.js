ef3a77b01502104615043549a9690a6a34662c43
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-07-05 10:55:21 -0400

    Include more information on each message in messageList

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,59 +1,42 @@
 import React from 'react'
-import { StyleSheet, css } from 'aphrodite'
 
-const RoomList = () => {
+const ChatHeader = () => {
   return (
-    <nav
-      className={`RoomList ${css(styles.roomList)}`}
+    <header
+      className="ChatHeader"
+      style={styles.chatHeader}
     >
-      <h2 className={css(styles.h2)}>Rooms</h2>
-      <ul className={css(styles.list)}>
-        <li>
-          <a href="#" className={css(styles.button)}>general</a>
-        </li>
-        <li>
-          <a href="#" className={css(styles.button)}>random</a>
-        </li>
-      </ul>
-    </nav>
+      <div className="roomInfo">
+        <h2 style={styles.h2}>
+          #general
+        </h2>
+        <p style={styles.p}>
+          Announcements and general chat
+        </p>
+      </div>
+    </header>
   )
 }
 
-const styles = StyleSheet.create({
-  roomList: {
-    padding: '0 1rem',
-  },
-
-  h2: {
-    fontSize: '1rem',
-  },
-
-  list: {
-    listStyle: 'none',
-    marginLeft: 0,
-    paddingLeft: 0,
-  },
-
-  heading: {
+const styles = {
+  chatHeader: {
+    backgroundColor: '#f3f3f3',
+    borderBottom: '1px solid #ccc',
+    padding: '0.5rem 1rem',
     display: 'flex',
-    justifyContent: 'space-between',
     alignItems: 'center',
   },
 
-  button: {
-    border: 0,
-    backgroundColor: 'transparent',
-    outline: 0,
-    padding: 0,
-    color: 'rgba(255,255,255,0.4)',
-    fontSize: '1rem',
-    cursor: 'pointer',
-    transition: 'color 0.25s ease-out',
-
-    ':hover': {
-      color: 'white',
-    }
+  h2: {
+    fontSize: '1.1rem',
+    margin: 0,
   },
-})
 
-export default RoomList
+  p: {
+    color: '#999',
+    margin: 0,
+    fontSize: '0.8rem',
+  }
+}
+
+export default ChatHeader
