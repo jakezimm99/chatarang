f3dc15018dfd28a76691c9765703f75f2a952ab8
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-07-05 12:00:47 -0400

    Saved Firebase

--- src/base.js
+++ src/base.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 
   const config = {
     apiKey: "AIzaSyDkupf2NGNbTwcNDBzwGxXXLirmD7Xp3XI",
@@ -7,4 +9,4 @@
     storageBucket: "",
     messagingSenderId: "147866628889"
   };
-  firebase.initializeApp(config);
+  const app = firebase.initializeApp(config);
