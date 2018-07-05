3dcfb1508801e34552b95857749ffc43fc39a908
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-07-05 09:28:39 -0400

    Reformatted user info and avatar included

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      loggedIn : false,
+      loggedIn : true,
       user :{
         uid : 'SDK3203jd',
         displayName : 'Jake',
---
+++ src/UserInfo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Avatar from './Avatar'
+
+const UserInfo = (props) => {
+    return (
+        <div className="UserInfo" style = {styles.userInfo}>
+        <Avatar user = {props.user}/>
+        <div className="user"
+        style = {styles.user}>{props.user.displayName}</div>
+        <a href="#">
+          <i className="fas fa-sign-out-alt"></i>
+        </a>
+      </div>
+    )
+}
+
+const styles = {
+    userInfo: {
+        padding: '0 1rem',
+        marginBottom: '1rem',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    user : {
+        flex: '1'
+    }
+}
+
+export default UserInfo
