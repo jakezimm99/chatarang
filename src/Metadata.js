d4a5b7d6aaf56a23b5cc484b8d534b8d63d5c674
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-07-11 09:34:41 -0400

    Add time messages were sent

--- src/Metadata.js
+++ src/Metadata.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Moment from 'moment'
 
 const Metadata = ({ message }) => {
   return (
@@ -10,7 +11,7 @@ const Metadata = ({ message }) => {
         {message.user.displayName}
       </div>
       <div style={styles.time}>
-        1:10 PM
+        {Moment(message.createdAt).fromNow()}
       </div>
     </div>
   )
